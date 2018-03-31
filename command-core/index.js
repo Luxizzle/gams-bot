7768d3bce622f29625eee217ea719df6c7197a4d
Jelmer <luxizzle@gmail.com>
2018-03-31 21:24:44 +0200

    templates?

--- command-core/index.js
+++ command-core/index.js
@@ -1,6 +1,24 @@
 const EventEmitter = require('events').EventEmitter;
 const Command = require('./command');
+const MessageTemplateBase = require('./template');
 const log = require('debug')('command-core');
+
+class DefaultTemplate extends MessageTemplateBase {
+  constructor(result) {
+    super();
+
+    this.setState({
+      authorMention: msg.author.mention,
+      result,
+    });
+  }
+
+  render(state, actions) {
+    return {
+      content: `${state.authorMention}, ${state.result}`,
+    };
+  }
+}
 
 class CommandCore extends EventEmitter {
   constructor(bot, options) {
@@ -24,9 +42,16 @@ class CommandCore extends EventEmitter {
     );
 
     this.commands = [];
+
+    this.defaultReply = DefaultTemplate;
   }
 
   add(cmd) {
+    if (cmd instanceof Command === false)
+      throw new Error(
+        `\`cmd\` should be an instance of Command, got \`${cmd.toString()}\``
+      );
+
     this.commands.push(cmd);
 
     return cmd;
@@ -41,10 +66,47 @@ class CommandCore extends EventEmitter {
     const prefix = this.options.prefixes.find(p => content.startsWith(p));
     if (!prefix) return;
 
-    log('[%s] Got prefix');
+    log('[%s] Got prefix', msg.id);
 
     // remove prefix from content
     content = content.substr(prefix.length - 1).trim();
+
+    // Find command
+    let label;
+    let command = this.commands.find(cmd => {
+      // Find label
+      label = cmd.labels.find(label => content.startsWith(label + ' '));
+      return label ? true : false;
+    });
+
+    // No command found
+    if (!command) return;
+
+    log('[%s] Got command %s', msg.id, label);
+
+    // Remove label from content
+    content = content.substr(label.length - 1).trim();
+
+    // Add parsed stuff to message
+    msg.command = { command, prefix, label, content };
+
+    this.execute(msg, msg.command);
+  }
+
+  async execute(msg, { command, content }) {
+    let channel = msg.channel;
+
+    let result = await msg.parse(msg, content);
+
+    if (result) {
+      if (result instanceof MessageTemplateBase) {
+        result.create(channel.createMessage);
+      } else {
+        let template = new DefaultTemplate(result);
+
+        template.create(channel.createMessage);
+      }
+    }
   }
 }
 
---
+++ command-core/template.js
@@ -0,0 +1,98 @@
+const log = require('debug')('message-template');
+const { DateTime } = require('luxon');
+const barbe = require('barbe');
+
+const p = require('../util/p');
+
+let msgContent = {
+  content: {
+    content: String,
+    embed: {
+      title: String,
+      description: String,
+      url: String,
+      timestamp: Date,
+      color: Number,
+      footer: {
+        text: String,
+        icon_url: String,
+      },
+      image: {
+        url: String,
+      },
+      thumbnail: {
+        url: String,
+      },
+      video: {
+        url: String,
+      },
+      provider: {
+        name: String,
+        url: String,
+      },
+      author: {
+        name: String,
+        url: String,
+        icon_url: String,
+        fields: [{ name: String, value: String, inline: Boolean }],
+      },
+    },
+  },
+  file: {
+    file: String,
+    name: String,
+  },
+};
+
+const defaultActions = {
+  now: () =>
+    DateTime.local().toLocaleString(DateTime.TIME_24_WITH_SHORT_OFFSET),
+};
+
+class MessageTemplateBase {
+  constructor(actions = {}) {
+    this.state = {};
+    this.actions = Object.assign({}, defaultActions, actions);
+  }
+
+  async create(createMessage) {
+    if (this.message) throw new Error('Template is already active');
+
+    let args = this._render();
+
+    let [err, message] = await p(createMessage(...args));
+    if (err) return err;
+
+    this.message = message;
+  }
+
+  setState(newState = {}) {
+    Object.assign({}, this.state, newState);
+
+    if (this.message) this._render();
+  }
+
+  static toArgs(data) {
+    return [data.content, data.file];
+  }
+
+  _render() {
+    let data = this.render(this.state, this.actions);
+
+    let args = MessageTemplateBase.toArgs(data);
+
+    if (this.message) {
+      this.message.edit(...args);
+    } else {
+      return args;
+    }
+  }
+
+  forceRender() {
+    this._render();
+  }
+
+  render() {}
+}
+
+module.exports = MessageTemplateBase;
